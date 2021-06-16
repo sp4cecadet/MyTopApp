c9363d51ea5695e3c4c82f71a294fc6db834de3a
sp4cecadet <iventus2020@gmail.com>
2021-06-16 09:40:03 +0300

    Some makeup

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,15 +1,12 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
 import cn from "classnames";
-import { Paragraph } from "../../components";
 import { format } from "date-fns";
 
 export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
 		<footer className={cn(className, styles.footer)} {...props}>
-			<div>
-				OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены
-			</div>
+			<div>OwlTop © {format(new Date(), "yyyy")} Все права защищены</div>
 			<a href="#" target="_blank">
 				Пользовательское соглашение
 			</a>
